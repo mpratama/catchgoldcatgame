72fd4d3d1fffa767d00fc16e6eb6d58fb76a5eaa
Arif Mulpratama <arifmul@gmail.com>
2021-05-16 16:04:16 +0700

    finished game, not polished yet

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -15,7 +15,6 @@ export default class Game extends Phaser.Scene {
 		this.load.scenePlugin('animatedTiles', AnimatedTiles, 'animatedTiles', 'animatedTiles');
 		this.load.tilemapTiledJSON('map', fieldMap);
 		this.load.image('tiles', atlas);
-		//this.load.image('smile', senyum);
 	}
 	
 	create(){
@@ -27,7 +26,14 @@ export default class Game extends Phaser.Scene {
 		this.layer2 = this.map.createLayer('vegetation', this.tileset, 0, 0);
 		this.sys.animatedTiles.init(this.map);
 		
-		this.groundShake = this.plugins.get('rexShakePosition').add(this.layer2, {
+		this.groundShake = this.plugins.get('rexShakePosition').add(this.layer1, {
+			mode: 1,
+			duration: 500,
+			magnitude: 1,
+			magnitudeMode: 1
+		});
+		
+		this.vegetationShake = this.plugins.get('rexShakePosition').add(this.layer2, {
 			mode: 1,
 			duration: 500,
 			magnitude: 1,
@@ -36,7 +42,8 @@ export default class Game extends Phaser.Scene {
 		
 		this.startRect = new Phaser.Geom.Rectangle(0, 0, 150, 260);
 		this.playArea = this.add.rectangle(0 , 0, 180, 275).setDisplayOrigin(0.5, 0.5).setInteractive();
-		//this.gold = this.physics.add.image(100, 100, 'smile');
+		
+		this.invisibleBorder = new Phaser.Geom.Rectangle(2, 1, 177, 267);
 		
 		this.cats = this.add.group();
 		
@@ -64,9 +71,10 @@ export default class Game extends Phaser.Scene {
             repeat: -1
         });
 		
-		for (var i = 0; i < 9; i++){
+		for (var i = 0; i < 3; i++){
 			this.cat = this.physics.add.sprite(null, null, 'cat', 2);
 			this.cat.setTint(Phaser.Math.RND.pick(this.colors));
+			//this.cat.body.setBoundsRectangle(this.invisibleBorder);
 			this.cats.add(this.cat);
 			this.cat.play('idle');
 		}
@@ -160,31 +168,111 @@ export default class Game extends Phaser.Scene {
 				this.points -= 50;
 				this.cat = this.physics.add.sprite(Phaser.Math.Between(this.pX - 10, this.pX + 10), Phaser.Math.Between(this.pY - 10, this.pY + 10), 'cat', 2);
 				this.cat.setTint(Phaser.Math.RND.pick(this.colors));
+				//this.cat.body.setBoundsRectangle(this.invisibleBorder);
 				this.cats.add(this.cat);
 				this.cat.play('idle');
 				this.lifeShake.shake();
 			}
 		});
 		
-		this.time.delayedCall(3000, this.spawnCoin, [], this);
+		this.time.delayedCall(8000, this.spawnGold, [], this);
+		this.time.delayedCall(5000, this.spawnMeteor, [], this);
+		this.time.delayedCall(7500, this.spawnMeteor2, [], this);
 		
 		
 	}
 	
-	spawnCoin(){
+	spawnMeteor(){
+		this.ang1 = Phaser.Math.Between(-180, 180);
+		this.spawnX = Phaser.Math.Between(20, 150);
+		this.spawnY = Phaser.Math.Between(-100, -200);
+		this.meteor = this.physics.add.sprite(this.spawnX, this.spawnY, 'cat', 4).setScale(1.5);
+		this.overlapMeteor = this.physics.add.overlap(this.cats, this.meteor, () => {
+			this.cameras.main.flashEffect.start(250, 219, 31, 72);
+			this.cats.getChildren()[0].destroy();	
+			this.meteor.destroy();
+			//this.timedEvent = this.time.delayedCall(2000, this.spawnGold, [], this);
+		}, null, this);
+		
+		this.tweens.add({
+			targets: this.meteor,
+			x: Phaser.Math.Between(20, 160),
+			y: Phaser.Math.Between(20, 260),
+			angle: this.ang1,
+			ease: 'Quad.easeIn',
+			duration: 1000,
+			onStart: () => {
+				this.overlapMeteor.active = false;
+			},
+			onComplete: () => {
+				this.overlapMeteor.active = true;
+				this.vegetationShake.shake();
+				this.groundShake.shake();
+				this.time.delayedCall(500, () => {
+					this.meteor.destroy();
+					this.spawnMeteor();
+				}, [], this);
+				//this.gold.setTint(0xf8d210);
+			},
+			onCompleteScope: this
+			//onCompleteParams: [ image ]
+		});
+		
+	}
+	
+	spawnMeteor2(){
+		this.ang2 = Phaser.Math.Between(-180, 180);
+		this.spawnX2 = Phaser.Math.Between(20, 150);
+		this.spawnY2 = Phaser.Math.Between(-100, -200);
+		this.meteor2 = this.physics.add.sprite(this.spawnX2, this.spawnY2, 'cat', 4).setScale(1.5);
+		this.overlapMeteor2 = this.physics.add.overlap(this.cats, this.meteor2, () => {
+			this.cameras.main.flashEffect.start(250, 219, 31, 72);
+			this.cats.getChildren()[0].destroy();	
+			this.meteor2.destroy();
+			//this.timedEvent = this.time.delayedCall(2000, this.spawnGold, [], this);
+		}, null, this);
+		
+		this.tweens.add({
+			targets: this.meteor2,
+			x: Phaser.Math.Between(20, 160),
+			y: Phaser.Math.Between(20, 260),
+			angle: this.ang2,
+			ease: 'Quad.easeIn',
+			duration: 1000,
+			onStart: () => {
+				this.overlapMeteor2.active = false;
+			},
+			onComplete: () => {
+				this.overlapMeteor2.active = true;
+				this.vegetationShake.shake();
+				this.groundShake.shake();
+				this.time.delayedCall(500, () => {
+					this.meteor2.destroy();
+					this.spawnMeteor2();
+				}, [], this);
+				//this.gold.setTint(0xf8d210);
+			},
+			onCompleteScope: this
+			//onCompleteParams: [ image ]
+		});
+		
+	}
+	
+	spawnGold(){
 		this.spawnX = Phaser.Math.Between(20, 150);
 		this.spawnY = Phaser.Math.Between(-10, -50);
 		this.gold = this.physics.add.sprite(this.spawnX, this.spawnY, 'cat', 4).setScale(1.5);
 		this.overlapCoin = this.physics.add.overlap(this.cats, this.gold, () => {
 			this.points += 10;
 			this.gold.destroy();
-			this.timedEvent = this.time.delayedCall(2000, this.spawnCoin, [], this);
+			this.timedEvent = this.time.delayedCall(2000, this.spawnGold, [], this);
 		}, null, this);
 		
 		this.tweens.add({
 			targets: this.gold,
 			x: Phaser.Math.Between(20, 160),
 			y: Phaser.Math.Between(20, 260),
+			angle: 90,
 			ease: 'Quint.easeIn',
 			duration: 1500,
 			onStart: () => {
@@ -193,6 +281,7 @@ export default class Game extends Phaser.Scene {
 			onComplete: () => {
 				this.overlapCoin.active = true;
 				this.groundShake.shake();
+				this.vegetationShake.shake();
 				this.gold.setTint(0xf8d210);
 			},
 			onCompleteScope: this
